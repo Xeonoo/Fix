99b5291a30207e7518d2c9c84f83b6415e65f0a3
Maksymilian <ts3joker@gmail.com>
2022-07-08 12:55:54 +0200

    table

--- Flix/Controllers/HomeViewController.swift
+++ Flix/Controllers/HomeViewController.swift
@@ -8,13 +8,46 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let homeFeesTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        view.addSubview(homeFeesTable)
+        
+        homeFeesTable.delegate = self
+        homeFeesTable.dataSource = self
     }
     
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeesTable.frame = view.bounds
+    }
+}
 
 
+extension  HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int ) -> Int {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for:  indexPath)
+        cell.textLabel?.text = "hello world"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+         return 40 
+    }
+    
 }
---
+++ Flix/Views/ColletionsViewTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ColletionsViewTableViewCell.swift
+//  Flix
+//
+//  Created by Maksymilian Patoń on 08/07/2022.
+//
+
+import UIKit
+
+class ColletionsViewTableViewCell: UITableViewCell {
+    
+    static let identifier = "ColletionsViewTableViewCell"
+    
+}
